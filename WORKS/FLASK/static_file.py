3caf5cf8fe42752441d96ec5874bf7068156409a
Noufan577 <noufankufi@gmail.com>
2026-02-10 19:01:40 +0530

    jinja

--- WORKS/FLASK/static_file.py
+++ WORKS/FLASK/static_file.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask import render_template
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    random_num=random.randint(1,100)
+    return render_template("index.html",num=random_num)
 
 
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
