f8899fc0ca80939b0a3b7653217a192121031a87
Noufan577 <noufankufi@gmail.com>
2026-02-17 16:43:54 +0530

    flask more

--- Projects/predictor/check.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-parm={
-        "name":"Abhi"
-    }
-response=requests.get(url="https://api.genderize.io",params=parm)
-data=response.json()
-print(data)
-print(data["gender"])
--- Projects/predictor/server.py
+++ Projects/predictor/server.py
@@ -1,50 +1,42 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 import requests
-
-def get_gender(name):
-    parm={
-        "name": name
-    }
-    response=requests.get(url="https://api.genderize.io",params=parm).json()
-
-    return response["gender"]
-
-def get_age(name):
-    parm={
-        "name":name
-    }
-    response=requests.get(url="https://api.agify.io",params=parm).json()
-
-    return response["age"]
-    
-
-
-
-    
 
 app = Flask(__name__)
 
+def get_gender(name):
+    response = requests.get(
+        "https://api.genderize.io",
+        params={"name": name}
+    ).json()
+    return response.get("gender")
+
+def get_age(name):
+    response = requests.get(
+        "https://api.agify.io",
+        params={"name": name}
+    ).json()
+    return response.get("age")
 
 @app.route("/")
 def home():
-    return "<b><h1>Welcome to my predictor</h1></b><br><br><h3>write you name in the url and gooooo!!!!!</h3>"
+    return render_template("index2.html")
 
-@app.route("/<name>")
-def page(name):
-    gender=get_gender(name)
-    age=get_age(name)
-    return render_template("index.html",uname=name,gender=gender,age=age)
+@app.route("/predict")
+def predict():
+    name = request.args.get("uname")
 
+    if not name:
+        return "Name not provided", 400
 
-if __name__=="__main__":
+    gender = get_gender(name)
+    age = get_age(name)
+
+    return render_template(
+        "index1.html",
+        uname=name,
+        gender=gender,
+        age=age
+    )
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-    
-    
-
-
-
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000)
-
